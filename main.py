11c129522ac33fe401bef99c45b28c9e78de1bd2
ash <ashshrawane123@gmail.com>
2023-02-23 12:48:19 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+    print("print via print line in quotes " + name)
+
+
+def input_print(input_user):
+    print(input_user)
+
+
+if __name__ == '__main__':
+    print_hi('AshFirst Code')
+
+    input_user = input("enter name")
+
+input_print(input_user)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
